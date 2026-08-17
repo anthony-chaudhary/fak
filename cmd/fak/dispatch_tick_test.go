fd570c4ab2336b70155412eeaa680f6ac2f1b6d0
fak test <fak@example.test>
2026-08-17 01:21:16 -0700

    test(dispatch): isolate worker-load debounce state (#6762) (fak cmd)
    Signed-off-by: fak test <fak@example.test>

--- cmd/fak/dispatch_tick_test.go
+++ cmd/fak/dispatch_tick_test.go
@@ -41,6 +41,9 @@ func withDispatchJSONHelper(t *testing.T, fn func(root string, args ...string) (
 	t.Setenv("FAK_HOST_RAM_MB_PER_WORKER", strconv.Itoa(dispatchtick.HostRAMMBPerWorker))
 	t.Setenv("FAK_HOST_THREADS_PER_CORE", strconv.Itoa(dispatchtick.HostThreadsPerCore))
 	t.Setenv("FAK_HOST_THREADS_PER_WORKER", strconv.Itoa(dispatchtick.HostThreadsPerWorker))
+	oldWorkerLoad := dispatchWorkerLoad
+	dispatchWorkerLoad = &dispatchWorkerLoadSignal{}
+	t.Cleanup(func() { dispatchWorkerLoad = oldWorkerLoad })
 	old := dispatchRunJSON
 	oldExternal := dispatchRunExternalJSON
 	oldResources := dispatchProbeHostResources
